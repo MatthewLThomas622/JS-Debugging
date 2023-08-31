1f3a23e785004590d6b7fba00c686dc3145977cb
Matthew Thomas <mlthomas622@gmail.com>
2023-08-31 17:51:39 -0500

    Added fortune.js/fortune.html

---
+++ fortune.js
@@ -0,0 +1,17 @@
+function tellFortune() {
+    let myArray = [
+        "You will succeed in all things",
+        "You will succeed in most things",
+        "You will succeed in some things",
+        "You will succeed in a few things",
+        "You will fail in a few things",
+        "You will fail in some things",
+        "You will fail in most things",
+        "You will fail in all things",
+        "You will win the lottery",
+        "You will get a promotion",
+    ];
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+document.body.innerHTML = randomItem;
+}
